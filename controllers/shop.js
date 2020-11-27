d9773461657f11f7692cac26e1b15c615983b134
nicoWeb31 <nico.riot@free.fr>
2020-11-27 18:52:36 +0100

    first commit

---
+++ controllers/shop.js
@@ -0,0 +1,60 @@
+const Product = require('../models/product');
+
+
+//get one
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+//get one
+exports.getOneProduct= (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId,product =>{
+
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: `${product.name}`,
+      path: '/product-detail'
+    })
+
+  })
+
+}
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
+
