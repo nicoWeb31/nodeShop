80cac1857f3de4e4ebf3d130e4c7ef46dd4aa1c1
nicoWeb31 <nico.riot@free.fr>
2020-12-01 18:48:22 +0100

    add mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,18 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const  MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
 
-module.exports = sequelize;
+const mongoConnect = (cb)=>{
+    
+    const db = process.env.DATABASE.replace('<pass>', process.env.DATABASE_PASSWORD);
+
+    MongoClient.connect(db)
+    .then(result=>{
+        console.log('connect with success 🙂 ');
+        cb(result)
+    })
+    .catch(err => console.log(err));
+
+}
+
+module.exports = mongoConnect
